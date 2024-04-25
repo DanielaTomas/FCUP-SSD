9808cea203345dd765846efd1d08334a1eb2a925
Diogo Nunes <diogoffn@gmail.com>
2024-04-25 17:21:25 +0100

    undid a woopsie

--- Public-Ledger-for-Auctions/src/main/java/BlockChain/BlockChainUtils.java
+++ Public-Ledger-for-Auctions/src/main/java/BlockChain/BlockChainUtils.java
@@ -1,19 +1,5 @@
 package BlockChain;
 
 public class BlockChainUtils {
-    /**
-     * Converts a byte array to a hexadecimal string representation.
-     *
-     * @param hash The byte array to be converted.
-     * @return The hexadecimal string representation of the byte array.
-     */
-    static String getHexString(byte[] hash) {
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : hash) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
-        }
-        return hexString.toString();
-    }
+
 }
--- Public-Ledger-for-Auctions/src/main/java/org/example/Utils.java
+++ Public-Ledger-for-Auctions/src/main/java/org/example/Utils.java
@@ -31,6 +31,22 @@ public class Utils {
     }
 
     /**
+     * Converts a byte array to a hexadecimal string representation.
+     *
+     * @param hash The byte array to be converted.
+     * @return The hexadecimal string representation of the byte array.
+     */
+    static String getHexString(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    /**
      * Serialize an object into a ByteBuf.
      *
      * @param obj The object to serialize.
